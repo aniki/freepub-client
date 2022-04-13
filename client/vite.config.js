83d00b9fcaca3f3e785f607564649a8d7fc809ef
Raphaël Nanni <raphael@MacBook-Pro-M1-Raphael.local>
2022-04-13 23:20:39 +0200

    PWA update

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,33 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 export default {
   plugins: [
-    VitePWA()
+    VitePWA({
+      manifest: {
+        name: "FREEPUB",
+        short_name: "Freepub",
+        theme_color: "#ffffff",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        icons: [
+          {
+            src: "icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    })
   ]
 }
