c55ba9960abb635c5814540b1eeef14b920d80f6
Raphaël Nanni <raphael@MacBook-Pro-M1-Raphael.local>
2022-04-14 21:32:28 +0200

    PWA manifest update

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,10 +6,10 @@ export default {
       manifest: {
         name: "FREEPUB",
         short_name: "Freepub",
-        theme_color: "#000",
+        theme_color: "#111",
         start_url: "/",
         display: "standalone",
-        background_color: "#ffffff",
+        background_color: "#111111",
         icons: [
           {
             src: "/icon-192.png",
